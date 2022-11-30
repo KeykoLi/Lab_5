7452a818e6cc5340b9d57cd8d19c72d7fd53d676
KeykoLi <119291961+KeykoLi@users.noreply.github.com>
2022-11-30 17:34:40 +0200

    Add Lab №5

---
+++ LAB 5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#pragma warning(default:4716)
+
+class Fighter
+{
+public:
+    string name;
+    int healthy;
+    int damagePerAttack;
+    Fighter(string Name, int Healthy, int DamagePerAttack)
+    {
+        name = Name;
+        healthy = Healthy;
+        damagePerAttack = DamagePerAttack;
+    };
+};
+
+class Fight
+{
+public:
+    void GetWinner(Fighter a, Fighter b)
+    {
+        if (a.healthy / b.damagePerAttack >= b.healthy / a.damagePerAttack)
+            cout << "Winner -- " << a.name << endl;
+        else
+            cout << "Winner -- " << b.name << endl;
+        return ;
+        
+    };
+};
+
+int main()
+{
+    Fighter Andry("A", 15, 4);
+    Fighter Dmitro("B", 20, 3);
+    Fight a;
+    a.GetWinner(N1, N2);
+}
